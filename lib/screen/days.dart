49049af34e141a46aed20b0f9884afbf1ecbd624
analpoly <analpoly127@gmail.com>
2024-07-24 10:41:04 +0530

    first

--- lib/screen/days.dart
+++ lib/screen/days.dart
@@ -1,104 +1,104 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_application_1/screen/image.dart';
-// import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screen/image.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-// class Days extends StatefulWidget {
-//   const Days({super.key});
+class Days extends StatefulWidget {
+  const Days({super.key});
 
-//   @override
-//   State<Days> createState() => _DaysState();
-// }
+  @override
+  State<Days> createState() => _DaysState();
+}
 
-// class _DaysState extends State<Days> {
-//   List list = [
-//     'ATHAM',
-//     'CHITHiRA',
-//     'CHODHI',
-//     'VISHAKAM',
-//     'ANIZHAM',
-//     'THRIKETA',
-//     'MOOLAM',
-//     'POORADAM',
-//     'UTHRADAM',
-//     'THIRUVONAM'
-//   ];
-//   List<DateTime> date = [
-//     DateTime(2024, 07, 23),
-//     DateTime(2024, 07, 24),
-//     DateTime(2024, 07, 25),
-//     DateTime(2024, 07, 26),
-//     DateTime(2024, 07, 27),
-//     DateTime(2024, 07, 28),
-//     DateTime(2024, 07, 29),
-//     DateTime(2024, 07, 30),
-//     DateTime(2024, 07, 31),
-//     DateTime(2024, 08, 01),
-//   ];
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Container(
-//         color: Colors.transparent,
-//         width: MediaQuery.of(context).size.width,
-//         height: MediaQuery.of(context).size.height,
-//         child: Column(
-//           children: [
-//             Expanded(
-//               child: ListView.builder(
-//                 itemCount: list.length,
-//                 itemBuilder: (context, index) {
-//                   DateTime datetime = date[index];
-//                   DateTime today = DateTime.now();
-//                   bool current = today.year == datetime.year &&
-//                       today.month == datetime.month &&
-//                       today.day == datetime.day;
+class _DaysState extends State<Days> {
+  List list = [
+    'ATHAM',
+    'CHITHiRA',
+    'CHODHI',
+    'VISHAKAM',
+    'ANIZHAM',
+    'THRIKETA',
+    'MOOLAM',
+    'POORADAM',
+    'UTHRADAM',
+    'THIRUVONAM'
+  ];
+  List<DateTime> date = [
+    DateTime(2024, 07, 23),
+    DateTime(2024, 07, 24),
+    DateTime(2024, 07, 25),
+    DateTime(2024, 07, 26),
+    DateTime(2024, 07, 27),
+    DateTime(2024, 07, 28),
+    DateTime(2024, 07, 29),
+    DateTime(2024, 07, 30),
+    DateTime(2024, 07, 31),
+    DateTime(2024, 08, 01),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.transparent,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: list.length,
+                itemBuilder: (context, index) {
+                  DateTime datetime = date[index];
+                  DateTime today = DateTime.now();
+                  bool current = today.year == datetime.year &&
+                      today.month == datetime.month &&
+                      today.day == datetime.day;
 
-//                   return Padding(
-//                     padding: const EdgeInsets.all(8.0),
-//                     child: GestureDetector(
-//                       onTap: () {
-//                         if (current) {
-//                           Navigator.of(context).push(MaterialPageRoute(
-//                               builder: (context) => Imagescreen()));
-//                         } else
-//                           (ScaffoldMessenger.of(context).showSnackBar(
-//                               SnackBar(content: Text('NOT ALLOWEDED'))));
-//                       },
-//                       child: Container(
-//                         height: 100,
-//                         decoration: BoxDecoration(
-//                             borderRadius: BorderRadius.circular(33),
-//                             gradient: LinearGradient(
-//                                 colors: current
-//                                     ? [
-//                                         // Color.fromARGB(130, 228, 6, 248),
-//                                         // Color.fromARGB(223, 133, 2, 145)
-//                                         Color.fromARGB(255, 255, 215, 0),
-//                                         Color.fromARGB(255, 255, 223, 186),
-//                                       ]
-//                                     : [
-//                                         Color.fromARGB(117, 255, 217, 0),
-//                                         Color.fromARGB(255, 255, 223, 186),
-//                                       ],
-//                                 begin: Alignment.centerLeft,
-//                                 end: Alignment.bottomLeft)),
-//                         child: Center(
-//                           child: ListTile(
-//                             leading: Text(
-//                               list[index],
-//                               style: GoogleFonts.inter(fontSize: 20),
-//                             ),
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                   );
-//                 },
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        if (current) {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => Imagescreen()));
+                        } else
+                          (ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text('NOT ALLOWEDED'))));
+                      },
+                      child: Container(
+                        height: 100,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(33),
+                            gradient: LinearGradient(
+                                colors: current
+                                    ? [
+                                        // Color.fromARGB(130, 228, 6, 248),
+                                        // Color.fromARGB(223, 133, 2, 145)
+                                        Color.fromARGB(255, 255, 215, 0),
+                                        Color.fromARGB(255, 255, 223, 186),
+                                      ]
+                                    : [
+                                        Color.fromARGB(117, 255, 217, 0),
+                                        Color.fromARGB(255, 255, 223, 186),
+                                      ],
+                                begin: Alignment.centerLeft,
+                                end: Alignment.bottomLeft)),
+                        child: Center(
+                          child: ListTile(
+                            leading: Text(
+                              list[index],
+                              style: GoogleFonts.inter(fontSize: 20),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screen/image.dart
+++ lib/screen/image.dart
@@ -1,74 +1,74 @@
-// import 'dart:io';
+import 'dart:io';
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter_application_1/screen/upload.dart';
-// import 'package:image_picker/image_picker.dart';
-// import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screen/upload.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-// class Imagescreen extends StatefulWidget {
-//   const Imagescreen({super.key});
+class Imagescreen extends StatefulWidget {
+  const Imagescreen({super.key});
 
-//   @override
-//   State<Imagescreen> createState() => _ImagescreenState();
-// }
+  @override
+  State<Imagescreen> createState() => _ImagescreenState();
+}
 
-// class _ImagescreenState extends State<Imagescreen> {
-//   ImagePicker _picker = ImagePicker();
-//   XFile? file;
-//   @override
-//   Widget build(BuildContext context) {
-//     Future<void> opencamera() async {
-//       if (await Permission.camera.request().isGranted) {
-//         final XFile? image =
-//             await _picker.pickImage(source: ImageSource.camera);
-//         if (image != null) {
-//           setState(() {
-//             file = image;
-//           });
-//           Navigator.of(context).push(MaterialPageRoute(
-//               builder: (context) => UploadScreen(image: File(image.path))));
-//         }
-//       }
-//     }
+class _ImagescreenState extends State<Imagescreen> {
+  ImagePicker _picker = ImagePicker();
+  XFile? file;
+  @override
+  Widget build(BuildContext context) {
+    Future<void> opencamera() async {
+      if (await Permission.camera.request().isGranted) {
+        final XFile? image =
+            await _picker.pickImage(source: ImageSource.camera);
+        if (image != null) {
+          setState(() {
+            file = image;
+          });
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => UploadScreen(image: File(image.path))));
+        }
+      }
+    }
 
-//     Future<void> openGallary() async {
-//       if (await Permission.photos.request().isGranted) {
-//         final XFile? images =
-//             await _picker.pickImage(source: ImageSource.gallery);
-//         if (images != null) {
-//           setState(() {
-//             file = images;
-//           });
-//           Navigator.of(context).push(MaterialPageRoute(
-//               builder: (context) => UploadScreen(image: File(images.path))));
-//         }
-//       }
-//     }
+    Future<void> openGallary() async {
+      if (await Permission.photos.request().isGranted) {
+        final XFile? images =
+            await _picker.pickImage(source: ImageSource.gallery);
+        if (images != null) {
+          setState(() {
+            file = images;
+          });
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => UploadScreen(image: File(images.path))));
+        }
+      }
+    }
 
-//     return Scaffold(
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           crossAxisAlignment: CrossAxisAlignment.center,
-//           children: [
-//             ElevatedButton.icon(
-//                 onPressed: () {
-//                   opencamera();
-//                 },
-//                 icon: Icon(Icons.camera_alt),
-//                 label: Text('OPEN CAMERA')),
-//             SizedBox(
-//               height: 20,
-//             ),
-//             ElevatedButton.icon(
-//                 onPressed: () {
-//                   openGallary();
-//                 },
-//                 icon: Icon(Icons.photo_library),
-//                 label: Text('Gallery')),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton.icon(
+                onPressed: () {
+                  opencamera();
+                },
+                icon: Icon(Icons.camera_alt),
+                label: Text('OPEN CAMERA')),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton.icon(
+                onPressed: () {
+                  openGallary();
+                },
+                icon: Icon(Icons.photo_library),
+                label: Text('Gallery')),
+          ],
+        ),
+      ),
+    );
+  }
+}
