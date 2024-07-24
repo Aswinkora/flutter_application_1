526b5b6c74b4132bb777fe65023bcf4036fe3111
analpoly <analpoly127@gmail.com>
2024-07-24 10:02:48 +0530

    first

--- lib/screen/upload.dart
+++ lib/screen/upload.dart
@@ -1,36 +1,36 @@
-import 'dart:io';
+// import 'dart:io';
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class UploadScreen extends StatefulWidget {
-  final File image;
-  const UploadScreen({super.key, required this.image});
+// class UploadScreen extends StatefulWidget {
+//   final File image;
+//   const UploadScreen({super.key, required this.image});
 
-  @override
-  State<UploadScreen> createState() => _UploadScreenState();
-}
+//   @override
+//   State<UploadScreen> createState() => _UploadScreenState();
+// }
 
-class _UploadScreenState extends State<UploadScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Center(
-              child: Container(
-                decoration: BoxDecoration(shape: BoxShape.circle),
-                height: 300,
-                width: 300,
-                child: Image.file(
-                  widget.image,
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+// class _UploadScreenState extends State<UploadScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SafeArea(
+//         child: Column(
+//           children: [
+//             Center(
+//               child: Container(
+//                 decoration: BoxDecoration(shape: BoxShape.circle),
+//                 height: 300,
+//                 width: 300,
+//                 child: Image.file(
+//                   widget.image,
+//                   fit: BoxFit.cover,
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
