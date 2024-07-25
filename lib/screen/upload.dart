763280fb643fbbde8df47976f9c51bd2dd1e4c98
Aswinkora <aswinaloor123@gmail.com>
2024-07-25 13:25:29 +0530

    updation

--- lib/screen/upload.dart
+++ lib/screen/upload.dart
@@ -3,6 +3,55 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/model/uploadimage.dart';
 
+// class UploadScreen extends StatefulWidget {
+//   final File image;
+//   const UploadScreen({super.key, required this.image});
+
+//   @override
+//   State<UploadScreen> createState() => _UploadScreenState();
+// }
+
+// class _UploadScreenState extends State<UploadScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//           image: DecorationImage(
+//               image: AssetImage('images/img2.png'), fit: BoxFit.cover)),
+//       child: Scaffold(
+//         backgroundColor: Colors.transparent,
+//         body: SafeArea(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Center(
+//                 child: Container(
+//                   decoration:
+//                       BoxDecoration(borderRadius: BorderRadius.circular(30)),
+//                   height: 300,
+//                   width: 300,
+//                   child: Image.file(
+//                     widget.image,
+//                     fit: BoxFit.cover,
+//                   ),
+//                 ),
+//               ),
+//               ElevatedButton(
+//                   onPressed: () {
+//                     upload();
+//                   },
+//                   child: Text('upload'))
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+
+//   void upload() {
+//     uploadimage(widget.image);
+//   }
+// }
 class UploadScreen extends StatefulWidget {
   final File image;
   const UploadScreen({super.key, required this.image});
@@ -12,12 +61,17 @@ class UploadScreen extends StatefulWidget {
 }
 
 class _UploadScreenState extends State<UploadScreen> {
+  bool _isUploading = false;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('images/img2.png'), fit: BoxFit.cover)),
+        image: DecorationImage(
+          image: AssetImage('images/img2.png'), 
+          fit: BoxFit.cover,
+        ),
+      ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: SafeArea(
@@ -26,8 +80,9 @@ class _UploadScreenState extends State<UploadScreen> {
             children: [
               Center(
                 child: Container(
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(30)),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
                   height: 300,
                   width: 300,
                   child: Image.file(
@@ -36,11 +91,18 @@ class _UploadScreenState extends State<UploadScreen> {
                   ),
                 ),
               ),
-              ElevatedButton(
-                  onPressed: () {
-                    upload();
-                  },
-                  child: Text('upload'))
+              SizedBox(height: 20),
+              _isUploading 
+                ? CircularProgressIndicator() 
+                : ElevatedButton(
+                    onPressed: _uploadImage,
+                    child: Text('Upload'),
+                  ),
+              if (_isUploading)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Uploading, please wait...'),
+                ),
             ],
           ),
         ),
@@ -48,7 +110,26 @@ class _UploadScreenState extends State<UploadScreen> {
     );
   }
 
-  void upload() {
-    uploadimage(widget.image);
+  void _uploadImage() async {
+    setState(() {
+      _isUploading = true;
+    });
+
+    try {
+      await uploadimage(widget.image);
+      setState(() {
+        _isUploading = false;
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Upload successful!')),
+      );
+    } catch (e) {
+      setState(() {
+        _isUploading = false;
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Upload failed: $e')),
+      );
+    }
   }
 }
